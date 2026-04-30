82b1ad02cd2d4c4112becfe0cd2d674ef9845b02
theusazevedd <matheus_ma02@hotmail.com>
2026-04-30 04:31:48 -0300

    Initial commit

---
+++ src/main/java/com/azevedo/barberflow/BarberflowApplication.java
@@ -0,0 +1,13 @@
+package com.azevedo.barberflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberflowApplication.class, args);
+	}
+
+}
